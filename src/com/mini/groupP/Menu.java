bca644e725ec39615ec09452e9c24406de4c4ca9
shreelondhe <shreedlondhe@gmail.com>
2023-05-27 14:51:16 +0530

    addded msg in menu

--- src/com/mini/groupP/Menu.java
+++ src/com/mini/groupP/Menu.java
@@ -6,6 +6,8 @@ public class Menu {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("welcome to ecommerse");
+		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("--------Welcome to Shoping Mart----------");
 		System.out.println();
