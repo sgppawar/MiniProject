f05478c4c726edf06e9b7215e27767e889c6804a
suraj pawar <surajpawar1432@gmail.com>
2023-05-27 15:03:58 +0530

    changes done

--- src/com/mini/groupP/Menu.java
+++ src/com/mini/groupP/Menu.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Menu {
 	
 	public static void main(String[] args) {
-		System.out.println("welcome to ecommerce");
 		
 		Scanner sc=new Scanner(System.in);
 		System.out.println("--------Welcome to Shoping Mart----------");
